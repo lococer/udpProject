cb7a50d4e3ae068940e23c9b8f337c5ce3ef294f
lococ <luozhiwei7@163.com>
2025-01-24 19:24:15 +0800

    初步client类实现，未测试

--- client/client.py
+++ client/client.py
@@ -2,6 +2,7 @@ import socket
 from common import common
 import random
 from typing import List
+import threading, time, hashlib
 
 server_ip = "47.98.224.148"  # 服务器的 IP 地址
 server_port = 12345  # 服务器的端口号
@@ -34,89 +35,159 @@ def send_udp_message(server_ip, server_port, message):
         client_socket.close()
 
 
-def readFile(path, size):
-    result = []
-    with open(path, "rb") as f:
-        while True:
-            content = f.read(size)
+class Client(common.FrameHandler):
 
-            if not content:
-                break
+    def __init__(self, server_ip, server_port, timeout=2, max_retries=3):
+        super().__init__()
+        self.__server_ip = server_ip
+        self.__server_port = server_port
+        self.timeout = timeout
+        self.mySocket.settimeout(self.timeout)
+        self.max_retries = max_retries
+        self.file = None
+        self.windowStart = 0  # 滑动窗口的起点
+        self.windowSize = 1  # 滑动窗口的大小
 
-            result.append(content)
+    def readFile(self, path, size) -> List[bytes]:
+        reslut = []
+        try:
+            with open(path, "rb") as f:
+                while True:
+                    content = f.read(size)
+                    if not content:
+                        break
+                    reslut.append(content)
+        except:
+            print("No such file!")
 
-            # print(f"read: {content}")
-    return result
+        self.fileMd5 = self.get_file_md5(path)
+        self.file = reslut
 
+    def get_file_md5(self, file_path):
+        """计算文件的 MD5 哈希值"""
+        hash_md5 = hashlib.md5()
+        try:
+            with open(file_path, "rb") as f:
+                for chunk in iter(lambda: f.read(4096), b""):
+                    hash_md5.update(chunk)
+            return hash_md5.digest()
+        except FileNotFoundError:
+            print(f"File not found: {file_path}")
+            return None
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None
 
-def send_with_timeout(client_socket,
-                      server_ip,
-                      server_port,
-                      encodeData,
-                      timeout=5,
-                      max_retries=3):
-    """
-    发送数据并处理超时重传
-    :param client_socket: 客户端套接字
-    :param server_ip: 服务器 IP 地址
-    :param server_port: 服务器端口号
-    :param encodeData: 要发送的数据列表
-    :param timeout: 超时时间（秒）
-    :param max_retries: 最大重试次数
-    """
-    client_socket.settimeout(timeout)  # 设置套接字超时时间
-
-    for i in range(len(encodeData)):
-        retries = 0
-        while retries < max_retries:
-            # print(f"传输 {i}")
-            common.sendUdpTo(client_socket, server_ip, server_port,
-                             encodeData[i])
+    def requestSend(self):
+        '''
+        请求发送文件,确保已经读过文件
+        '''
+        assert self.file is not None
+        tries = 0
+        while tries < self.max_retries:
+            request = self.myEncode(0, common.REQUEST, len(self.file),
+                                    self.fileMd5)
+            self.sendUdp(self.__server_ip, self.__server_port, request)
             try:
-                response, _ = client_socket.recvfrom(10240)  # 尝试接收响应
-                # print(f"收到响应: {response}")
-                break  # 成功接收到响应，跳出重试循环
-            except socket.timeout:
-                print(f"超时，正在重试 {retries + 1}/{max_retries}")
-                retries += 1
+                response, address = self.receiveUdp()
+                if response.getRequestCode is common.ALLOW:
+                    try:
+                        self.Id = response.getId()
+                        self.fileSend()
+                    except Exception as e:
+                        print(e)
+                elif response.getRequestCode is common.DENY:
+                    print(f"Server denied")
+                    return
+                else:
+                    raise Exception(
+                        f"Except an ALLOW or DENY, but a {response.getRequestCode}"
+                    )
+
+            except socket.timeout as e:
+                tries += 1
+                continue
+            except socket.error as e:
+                socket.error(f"socket error: {e}")
         else:
-            print(f"传输 {i} 失败，达到最大重试次数")
-            # 可以在这里处理失败情况，例如记录日志或抛出异常
+            print(f"Can't connect to server")
+            return
+
+    def listenACK(self):
+        '''
+        监听ACK,更新滑动窗口
+        '''
+        tries = 0
+        while self.windowStart < len(self.fileBytes):
+            tries += 1
+            if tries >= self.max_retries * 10:  # 发文件宽松点
+                raise Exception("long time don't receive response from server")
+
+            try:
+                response, address = self.receiveUdp()
+            except socket.timeout as e:
+                continue
+
+            if response.getRequestCode is not common.ACK:  # 忽略非ACK
+                continue
+
+            currentACK = response.getData
+
+            if (currentACK < self.windowStart
+                    or currentACK >= self.windowStart + self.windowSize):
+                continue
+
+            tries = 0  # 接收到有效ACK
+            self.receivedACK.add(currentACK)
+            while (self.windowStart in self.receivedACK):  # 更新窗口位置
+                self.windowStart += 1
+                if self.windowStart >= len(self.fileBytes): break  # 中止条件
+
+    def sendWindow(self):
+        '''
+        定时发送window里的所有数据
+        '''
+        while True:
+            with self.lock:  # 锁发送区间
+                l = self.windowStart
+                r = min(l + self.windowSize, len(self.fileBytes))
+
+            if l >= len(self.fileBytes): return  # 中止条件
+
+            for i in range(l, r):
+                self.sendUdp(self.__server_ip, self.__server_port,
+                             self.fileBytes[i])
+                time.sleep(0.001)  # 两个间隔1ms
+
+            time.sleep(0.01)  # 两次发送间隔10ms
+
+    def fileSend(self):
+        self.receivedACK = set()
+        self.fileBytes = self.myEncodeData(self.Id, common.DATA, self.file)
+        self.lock = threading.Lock()
+        thread1 = threading.Thread(self.listenACK)
+        thread2 = threading.Thread(self.sendWindow)
+
+        thread1.start()
+        thread2.start()
+
+        thread1.join()
+        thread2.join()
+
+        # 清理工作
+        self.file = None
+        del self.receivedACK
+        del self.fileBytes
+        del self.lock
 
 
 # 示例使用
 if __name__ == "__main__":
 
-    data = readFile("./client/bible.txt", 1200)
-    # print(data)
-    encodeData = common.myEncodeData(random.randint(0, 1000), common.DATA,
-                                     data)
-    # print(encodeData, hex)
-    # for i in encodeData:
-    #     for byte in i:
-    #         print(f"{byte:02x}", end=" ")
-    #     print()
+    client = Client(server_ip, server_port)
 
-    encodeRequest = common.myEncode(random.randint(0, 1000), common.REQUEST,
-                                    10)
-    # for byte in encodeRequest:
-    #     print(f"{byte:02x}", end=' ')
-    # print()
+    client.readFile("./client/bible.txt", 1200)
 
-    # 创建一个 UDP 套接字
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    common.sendUdpTo(client_socket, server_ip, server_port, encodeRequest)
-    print(f"已发送: {encodeRequest}")
-    response = client_socket.recvfrom(10240)
-    transmitId = common.getId(response[0])
-    print(f"从服务器收到响应: {transmitId}")
+    client.requestSend()
 
-    encodeData = common.myEncodeData(transmitId, common.DATA, data)
-
-    print(f"总大小:{len(encodeData)},正在传输...")
-    print(f"第一帧大小:{len(encodeData[0])}")
-
-    # 调用发送函数
-    send_with_timeout(client_socket, server_ip, server_port, encodeData)
-
-    print("传输完成")
+    print("程序结束")
--- common/common.py
+++ common/common.py
@@ -1,6 +1,7 @@
 from typing import List
 import socket
 import ipaddress
+import threading
 
 REQUEST = 1
 ALLOW = 2
@@ -11,6 +12,66 @@ RETS = 6
 
 # 最大传输编号
 MAXID = 1000
+
+
+class Frame:
+
+    def __init__(self):
+        self.frame = None
+
+    def setFrame(self, bytes: bytes):
+        assert (len(bytes) >= 20)
+        self.frame = bytes
+
+    def getFrame(self) -> bytes:
+        assert (self.frame is not None)
+        return self.frame
+
+    def calculateCheckCode(self) -> bytes:
+        '''
+        忽略校验位计算校验码
+        输入为bytes
+        使用奇偶校验
+        TODO 换成哈希
+        '''
+        assert (self.frame is not None)
+        result = 0
+        for i in self.data:
+            result ^= i
+        for i in self.data[12:16]:
+            result ^= i
+        return result.to_bytes(4, 'little')
+
+    def verifyCheckCode(self, data):
+        '''
+        验算校验码是否正确
+        '''
+        assert (self.frame is not None)
+        checkCode = data[12:16]
+        return calculateCheckCode(data) == checkCode
+
+    def getRequestCode(self) -> bytes:
+        assert (self.frame is not None)
+        return BtoInt(self.data[4:8])
+
+    def getId(self) -> int:
+        assert (self.frame is not None)
+        return BtoInt(self.data[0:4])
+
+    def getSize(self) -> int:
+        assert (self.frame is not None)
+        return BtoInt(self.data[16:20])
+
+    def getData(self) -> str:
+        assert (self.frame is not None)
+        return self.data[20:20 + self.getSize()].decode('utf-8')
+
+
+def BtoInt(bytes: bytes) -> int:
+    '''
+    将4B转为int
+    '''
+    return int.from_bytes(bytes, 'little')
 
 
 def myEncode(id: int, requestCode: int, data: int = 0) -> bytes:
@@ -104,13 +165,6 @@ def myEncodeData(id: int, requestCode: int, data: List[bytes]) -> List[bytes]:
     return encodeData
 
 
-def myDecode(data):
-    '''
-    实现解码部分，
-    '''
-    pass
-
-
 def calculateCheckCode(data):
     '''
     忽略校验位计算校验码
@@ -194,6 +248,151 @@ def sendUdpTo(mySocket: socket, ip: str, port: int, frameBytes: bytes):
     except socket.error as e:
         raise socket.error(
             f"Failed to send UDP packet to {ip}:{port}. Error: {e}")
+
+
+class FrameHandler:
+
+    def __init__(self, ):
+        self.mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def sendUdp(self, ip: str, port: int, frameBytes: bytes):
+        '''
+        使用mySocket发送UDP到ip:port
+        '''
+        # 校验 IP 地址格式
+        try:
+            ipaddress.ip_address(ip)
+        except ValueError:
+            raise ValueError(f"Invalid IP address: {ip}")
+
+        # 校验端口号范围
+        if not (0 <= port <= 65535):
+            raise ValueError(
+                f"Invalid port number: {port}. Port must be between 0 and 65535."
+            )
+
+        # 校验 frameBytes 类型
+        if not isinstance(frameBytes, bytes):
+            raise TypeError("frameBytes must be of type bytes.")
+
+        # 校验 frameBytes 长度
+        if len(frameBytes) > 65507:
+            raise ValueError(
+                "frameBytes is too large. UDP datagrams should not exceed 65507 bytes."
+            )
+
+        try:
+            self.mySocket.sendto(frameBytes, (ip, port))
+        except socket.timeout as e:
+            raise e
+        except socket.error as e:
+            raise socket.error(
+                f"Failed to send UDP packet to {ip}:{port}. Error: {e}")
+
+    def receiveUdp(self) -> tuple[Frame, any]:
+        try:
+            data, address = self.mySocket.recvfrom(10240)
+            frame = Frame()
+            frame.setFrame(data)
+            return frame, address
+        except socket.timeout as e:
+            raise e
+        except socket.error as e:
+            socket.error(f"socket error: {e}")
+
+    def myEncode(self,
+                 id: int,
+                 requestCode: int,
+                 data1: int = 0,
+                 data2: int = 0) -> bytes:
+        '''
+        实现编码部分，自定义格式，用于非数据编码
+        4B 传输编号 id 
+        4B 请求码 requestCode 
+        4B 序列号 sequence
+        4B 校验码 checkCode
+        4B 数据长度 dataSize
+            数据 data
+        输入为 传输编号id, 请求码 requestCode, 数据data
+        输出为构造好的byte
+        '''
+        encodeData = None
+
+        # 传输id
+        idBytes = (id.to_bytes(4, 'little'))
+
+        #请求码
+        requestCodeBytes = ((requestCode).to_bytes(4, 'little'))
+
+        #序列号
+        sequenceBytes = data1.to_bytes(4, 'little')
+
+        #校验码
+        checkCodeBytes = b'\x00\x00\x00\x00'
+
+        #数据长度
+        dataSizeBytes = b'\x00\x00\x00\x00'
+
+        #数据
+        dataBytes = bytes()
+
+        frameBytes = idBytes + requestCodeBytes + sequenceBytes + checkCodeBytes + dataSizeBytes + dataBytes
+
+        frameByteArray = bytearray(frameBytes)
+
+        frameByteArray[12:16] = calculateCheckCode(frameBytes)
+
+        frameBytes = bytes(frameByteArray)
+
+        return frameBytes
+
+    def myEncodeData(self, id: int, requestCode: int,
+                     data: List[bytes]) -> List[bytes]:
+        '''
+        实现编码部分，自定义格式，仅用于编码数据
+        4B 传输编号 id 
+        4B 请求码 requestCode 
+        4B 序列号 sequence
+        4B 校验码 checkCode
+        4B 数据长度 dataSize
+            数据 data
+        输入为 传输编号id, 请求码 requestCode, 数据data
+        输出为构造好的list
+        '''
+        encodeData = []
+
+        # 传输id
+        idBytes = (id.to_bytes(4, 'little'))
+
+        #请求码
+        requestCodeBytes = ((requestCode).to_bytes(4, 'little'))
+
+        for index, value in enumerate(data):
+            # 序列号
+            sequenceBytes = index.to_bytes(4, 'little')
+
+            # 校验码
+            checkCodeBytes = b'\x00\x00\x00\x00'
+
+            # 数据长度
+            dataSizeBytes = len(value).to_bytes(4, 'little')
+
+            # 数据
+            dataBytes = value
+
+            frameBytes = idBytes + requestCodeBytes + sequenceBytes + checkCodeBytes + dataSizeBytes + dataBytes
+
+            frameByteArray = bytearray(frameBytes)
+
+            frameByteArray[12:16] = calculateCheckCode(frameBytes)
+
+            frameBytes = bytes(frameByteArray)
+
+            assert verifyCheckCode(frameBytes)
+
+            encodeData.append(frameBytes)
+
+        return encodeData
 
 
 if __name__ == '__main__':
